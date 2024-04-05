2ad3ed5a909b354afe2a3342b4068e97cfba899c
Muskan Thakur <mt230046@gmail.com>
2024-04-05 17:26:34 +0530

    workibng but side bar deleted

---
+++ task-management/app/components/sidebar/sidebar.tsx
@@ -0,0 +1,89 @@
+"use client";
+import React from "react";
+import styled from "styled-components"
+import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
+
+    function Sidebar(){
+        const {theme} = useGlobalState();
+        return(
+            <SidebarStyled theme={theme}>
+                <div className="profile">
+                    <div className="profile-overlay">
+                      <Image width={70} height={70} src="/girlimg1.jpg" alt="profile"/>
+                    </div>
+                    <h1>
+                        <span>Sin</span>
+                        <span>Rostro</span>
+                    </h1>
+
+                </div>
+                <ul className="nav-items">
+                    {menu.map((item)=>{
+                        return(
+                            <li>
+                                
+                            </li>
+                        )
+                    }
+                    
+
+                </ul>
+
+            </SidebarStyled>
+        );
+
+    }
+    
+      export default Sidebar;       
+            
+        
+      
+
+
+
+        
+        
+   
+
+
+   
+    
+    
+    
+              
+    
+
+
+
+   
+    
+       
+       
+        
+
+
+                
+ 
+        
+        
+       
+                
+                
+              
+            
+          
+      
+          
+  
+   
+    
+     
+   
+       
+
+ 
+
+   
+    
+           
